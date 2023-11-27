f027988530d82c4abc44266a6a15a5c35d7083ba
Galoniax <axelian2504@gmail.com>
2023-11-27 14:25:36 -0300

    refactor: update Shape class.

--- Shapes/src/Shape.java
+++ Shapes/src/Shape.java
@@ -1,4 +1,39 @@
 public abstract class Shape {
+    private String color;
+    private boolean filled;
+
+    //Constructor
+
+    public Shape() {
+        this.color = "red";
+        this.filled = true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
 
 
+    //Getters and Setters
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    //Abstracts
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
 }
