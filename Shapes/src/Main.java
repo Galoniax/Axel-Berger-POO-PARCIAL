1791dcbbe44ccd52185c451a766f5debf39c878f
Galoniax <axelian2504@gmail.com>
2023-11-27 15:58:59 -0300

    feat: add objects to Main

--- Shapes/src/Main.java
+++ Shapes/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shape s1 = new Circle(5.5, "red", false);
+        Shape s3 = new Rectangle(1.0, 2.0, "red", false);
+        Shape s4 = new Square(6.6);
+
     }
 }
