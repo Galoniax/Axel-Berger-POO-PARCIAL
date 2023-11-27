127128eb417da316c73985537c104e94257f19ed
Galoniax <axelian2504@gmail.com>
2023-11-27 14:28:16 -0300

    feat: create Circle class.

---
+++ Shapes/src/Circle.java
@@ -0,0 +1,25 @@
+public abstract class Circle extends Shape{
+    private double radius;
+
+    //Constructors
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
